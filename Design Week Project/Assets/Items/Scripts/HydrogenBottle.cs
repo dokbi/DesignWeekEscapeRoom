989cf0b9649a7407a9570728b8dccccc62f545a2
Qiao <qiaokuanghua@gmail.com>
2022-03-10 18:49:11 -0500

    Chemistry Puzzle Functional

--- Design Week Project/Assets/Items/Scripts/HydrogenBottle.cs
+++ Design Week Project/Assets/Items/Scripts/HydrogenBottle.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class HydrogenBottle : MonoBehaviour, IUsable
 {
     public GameObject acidBottle;
+    public float lauchForce = 125f;
+    private Rigidbody bottleRb;
+    private void Awake()
+    {
+        bottleRb = GetComponent<Rigidbody>();
+    }
+
     public void Use(ItemHandler _owner)
     {
+        Vector3 facing = _owner.itemAnchor.transform.forward;
         _owner.Drop();
+        bottleRb.AddForce(facing * lauchForce, ForceMode.Impulse);
+        bottleRb.AddTorque(Vector3.right * lauchForce, ForceMode.Impulse);
     }
 
     private void OnCollisionEnter(Collision collision)
