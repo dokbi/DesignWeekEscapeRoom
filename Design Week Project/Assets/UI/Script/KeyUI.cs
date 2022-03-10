79fd89c37b7bd907b1cc0f98a72c7caa33b03805
Qiao <qiaokuanghua@gmail.com>
2022-03-10 16:31:00 -0500

    Fix UI

--- Design Week Project/Assets/UI/Script/KeyUI.cs
+++ Design Week Project/Assets/UI/Script/KeyUI.cs
@@ -9,16 +9,16 @@ public class KeyUI : MonoBehaviour
     // Start is called before the first frame update
     private void Awake()
     {
-        keys = new GameObject[2];
+        keys = new GameObject[3];
         keys[0] = transform.GetChild(0).gameObject;
         keys[1] = transform.GetChild(1).gameObject;
-        keys[2] = transform.GetChild(1).gameObject;
+        keys[2] = transform.GetChild(2).gameObject;
     }
     void Start()
     {
         keys[0].SetActive(false);
         keys[1].SetActive(false);
-        keys[3].SetActive(false);
+        keys[2].SetActive(false);
     }
 
     public void EnableKey(int keyNumber)
