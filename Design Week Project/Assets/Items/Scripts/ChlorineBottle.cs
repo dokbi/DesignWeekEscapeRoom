5e3677de2e93fb7e1bbfc37e5c003c2e74bd7e89
Qiao <qiaokuanghua@gmail.com>
2022-03-10 18:49:41 -0500

    Chemistry puzzle fix

--- Design Week Project/Assets/Items/Scripts/ChlorineBottle.cs
+++ Design Week Project/Assets/Items/Scripts/ChlorineBottle.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class ChlorineBottle : MonoBehaviour, IUsable
 {
+    public float lauchForce = 125f;
+    private Rigidbody bottleRb;
+    private void Awake()
+    {
+        bottleRb = GetComponent<Rigidbody>();
+    }
+
     public void Use(ItemHandler _owner)
     {
+        Vector3 facing = _owner.itemAnchor.transform.forward;
         _owner.Drop();
+        bottleRb.AddForce(facing * lauchForce, ForceMode.Impulse);
+        bottleRb.AddTorque(Vector3.right * lauchForce, ForceMode.Impulse);
     }
 
     private void OnCollisionEnter(Collision collision)
